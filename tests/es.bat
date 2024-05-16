99eecb83306f6474fd3b82e38a906069bf361bf5
Alexandr Martynov <mag@unicont.com>
2024-05-16 15:53:39 +0300

    Обновил ES тест

--- tests/es.bat
+++ tests/es.bat
@@ -9,12 +9,16 @@
 @call setTestConfig.bat
 @call copyConf.bat
 
-@set VERBOSE=-V=9
+
+@rem roboconf.exe  --make-dump -V=9 --log-source-code-pos -V=-conf-dump-short-rules-cls -V=-conf-dump-rules-cls -V=-data-dump-short -V=-net-chkstp-not-matched --report=periph   --rules=es.rul  doc\schematic\ES-4007-BMCL-CPU_R.1.0.NET  doc.generated\ES-4007-BMCL-CPU_R_1_0_periph.html
+
+
+@set VERBOSE=-V=9 
 @rem set VERBOSE=%VERBOSE% -V=-search-lib-log -V=-search-lib-summary
 @rem set VERBOSE=%VERBOSE% -V=-net-dump-pins -V=-net-dump-nets -V=-net-dump-cmn -V=-net-dump-cmps
 @rem set VERBOSE=%VERBOSE% -V=-cls-chk-not-matched -V=-cls-chk-matched
 @rem set VERBOSE=%VERBOSE% -V=-pkg-known
-@set VERBOSE=%VERBOSE% --log-source-code-pos
+@set VERBOSE=%VERBOSE% --log-source-code-pos --make-dump
 
 @call mk_out_dirs.bat
 @if not exist es @mkdir es
@@ -30,7 +34,6 @@
 @rem periph\jud_TriggersControl_v2_3_Driver
 %ROBOCONF% %VERBOSE% --make-dump --report=h_conf  --local-lib-cache=lib-cache\es.rbc_libs --rules=rules\es.rul  net-cache\es.rbc_net  es_conf
 @rem h_conf\jud_TriggersControl_v2_3_Driver
-
 
 
 
