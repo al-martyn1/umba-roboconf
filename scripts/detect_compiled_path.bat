751fd35f55904cc942309da02235279b3f630b6f
al-martyn1 <amart@mail.ru>
2024-06-11 23:07:12 +0300

    Сделал проекты/солюшены для MSVC2019/2022. Поправил скрипт детекта бинарей

--- scripts/detect_compiled_path.bat
+++ scripts/detect_compiled_path.bat
@@ -1,35 +1,63 @@
 
-:TRY_MSVC_RELEASE
-@if not exist %~dp0\..\.out\msvc\Win32\Release\umba-roboconf.exe goto TRY_MSVC_DEBUG
-@set RBC_COMPILED_BIN_PATH=%~dp0\..\.out\msvc\Win32\Release
+:TRY_MSVC2022_RELEASE
+@if not exist %~dp0\..\.out\msvc2022\Win32\Release\umba-roboconf.exe goto TRY_MSVC2022_DEBUG
+@set RBC_COMPILED_BIN_PATH=%~dp0\..\.out\msvc2022\Win32\Release
 @goto DO_COPY
 
 
-:TRY_MSVC_DEBUG
-@if not exist %~dp0\..\.out\msvc\Win32\Debug\umba-roboconf.exe goto TRY_GCC_V1_RELEASE
-@set RBC_COMPILED_BIN_PATH=%~dp0\..\.out\msvc\Win32\Debug
+:TRY_MSVC2022_DEBUG
+@if not exist %~dp0\..\.out\msvc2022\Win32\Debug\umba-roboconf.exe goto TRY_MSVC2019_RELEASE
+@set RBC_COMPILED_BIN_PATH=%~dp0\..\.out\msvc2022\Win32\Debug
 @goto DO_COPY
 
 
-:TRY_GCC_V1_RELEASE
-@if not exist "%~dp0\..\.build\GCC 7.3.0 i686-w64-mingw32\win32-x86\Release\umba-roboconf.exe" goto TRY_GCC_V1_DEBUG
+
+:TRY_MSVC2019_RELEASE
+@if not exist %~dp0\..\.out\msvc2019\Win32\Release\umba-roboconf.exe goto TRY_MSVC2019_DEBUG
+@set RBC_COMPILED_BIN_PATH=%~dp0\..\.out\msvc2019\Win32\Release
+@goto DO_COPY
+
+
+:TRY_MSVC2019_DEBUG
+@if not exist %~dp0\..\.out\msvc2019\Win32\Debug\umba-roboconf.exe goto TRY_MSVC2017_RELEASE
+@set RBC_COMPILED_BIN_PATH=%~dp0\..\.out\msvc2019\Win32\Debug
+@goto DO_COPY
+
+
+
+:TRY_MSVC2017_RELEASE
+@if not exist %~dp0\..\.out\msvc2017\Win32\Release\umba-roboconf.exe goto TRY_MSVC2017_DEBUG
+@set RBC_COMPILED_BIN_PATH=%~dp0\..\.out\msvc2017\Win32\Release
+@goto DO_COPY
+
+
+:TRY_MSVC2017_DEBUG
+@if not exist %~dp0\..\.out\msvc2017\Win32\Debug\umba-roboconf.exe goto TRY_GCC_V7_3_V1_RELEASE
+@set RBC_COMPILED_BIN_PATH=%~dp0\..\.out\msvc2017\Win32\Debug
+@goto DO_COPY
+
+
+
+
+:TRY_GCC_V7_3_V1_RELEASE
+@if not exist "%~dp0\..\.build\GCC 7.3.0 i686-w64-mingw32\win32-x86\Release\umba-roboconf.exe" goto TRY_GCC_V7_3_V1_DEBUG
 @set RBC_COMPILED_BIN_PATH=%~dp0\..\.build\GCC 7.3.0 i686-w64-mingw32\win32-x86\Release
 @goto DO_COPY
 
 
-:TRY_GCC_V1_DEBUG
-@if not exist "%~dp0\..\.build\GCC 7.3.0 i686-w64-mingw32\win32-x86\Debug\umba-roboconf.exe" goto TRY_GCC_V2_DEBUG
+:TRY_GCC_V7_3_V1_DEBUG
+@if not exist "%~dp0\..\.build\GCC 7.3.0 i686-w64-mingw32\win32-x86\Debug\umba-roboconf.exe" goto TRY_GCC_V7_3_V2_RELEASE
 @set RBC_COMPILED_BIN_PATH=%~dp0\..\.build\GCC 7.3.0 i686-w64-mingw32\win32-x86\Debug
 @goto DO_COPY
 
 
-:TRY_GCC_V2_RELEASE
-@if not exist "%~dp0\..\.build\GCC-73_32-QT\none-unknown\Release\umba-roboconf.exe" goto TRY_GCC_V2_DEBUG
+:TRY_GCC_V7_3_V2_RELEASE
+@if not exist "%~dp0\..\.build\GCC-73_32-QT\none-unknown\Release\umba-roboconf.exe" goto TRY_GCC_V7_3_V2_DEBUG
 @set RBC_COMPILED_BIN_PATH=%~dp0\..\.build\GCC-73_32-QT\none-unknown\Release
 @goto DO_COPY
 
 
-:TRY_GCC_V2_DEBUG
+:TRY_GCC_V7_3_V2_DEBUG
 @if not exist "%~dp0\..\.build\GCC-73_32-QT\none-unknown\Debug\umba-roboconf.exe" goto ERR_EXE
 @set RBC_COMPILED_BIN_PATH=%~dp0\..\.build\GCC-73_32-QT\none-unknown\Debug
 @goto DO_COPY
